ee5592679d6e8947c7edf0342e0b7f7900f574a4
IdaliaGpe <idaliaguadalupepucca@hotmail.com>
2020-08-28 09:01:20 -0700

    node

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,12 @@
 #include<iostream>
+#include"Node.hh"
 
 void printValue(int& value)
 {
     std::cout << "memmory of value: " << &value << " value inside value: " << value << std::endl;
 }
 
-struct Person
+/*struct Person
 {
     const char* name;
     int age;
@@ -17,11 +18,28 @@ struct Person
         this->age = age;
         this->height = height;
     }
-};
+};*/
 
 int main()
 {
-    int something{5}; //stack
+    Node* node0 = new Node(0, nullptr);
+    node0->nextNode = new Node(1, nullptr);
+    node0->nextNode->nextNode = new Node(2, nullptr);
+    node0->nextNode->nextNode->nextNode = new Node(3, nullptr);
+    node0->nextNode->nextNode->nextNode->nextNode = new Node(4, nullptr);
+
+    //std::cout << node0->value << std::endl;
+    //std::cout << node0->nextNode-> value << std::endl;
+
+    Node* currentNode = node0;
+
+    while (currentNode)
+    {
+        std::cout << currentNode->value << "->" << "\t";
+        currentNode = currentNode->nextNode;
+    }
+    
+    /*int something{5}; //stack
 
     int& ref{something}; //esto es una referencia (variable por referencia)
 
@@ -30,14 +48,31 @@ int main()
     ref++;
 
     int* ptr1{&something}; //Puntero
+    
+    ++*ptr1;*/
 
-    Person* person1{new Person("Jose", 19, 1.60)};
+    /*Person* arrPointer[]
+    {
+        new Person("Jose", 19, 1.60),
+        new Person("Maria", 25, 1.59)
+    };
+
+     for(Person*& p : arrPointer)
+    {
+        std::cout << "value of person: " << p << std::endl;
+        std::cout << "name of person: " << p->name << std::endl;
+        std::cout << "age of person: " << p->age << std::endl;
+        std::cout << "height of person: " << p->height << std::endl;
+        std::cout << std::endl;
+    }*/
+
+    /*Person* person1{new Person("Jose", 19, 1.60)};
 
     std::cout << "memmory of person1: " << &person1 << std::endl;
     std::cout << "value of person1: " << person1 << std::endl;
     std::cout << "name of person1: " << &person1->name << std::endl;
-    std::cout << "age of person1: " << person1->age << std::endl;ls
-    std::cout << "height of person1: " << person1->height << std::endl;
+    std::cout << "age of person1: " << person1->age << std::endl;
+    std::cout << "height of person1: " << person1->height << std::endl;*/
 
     /*std::cout << "memmory of something: " << &something << " value inside memmory: " << something << std::endl;
     std::cout << "memmory of ref: " << &ref << " value inside ref: " << ref << std::endl;
